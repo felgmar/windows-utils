73256846ae0513853c58ec0d8ea5a5d165f00a10
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2021-09-07 21:00:58 +0200

    Improved image apply and script behaviour

--- wincli-it.bat
+++ wincli-it.bat
@@ -6,17 +6,15 @@ title Windows CLI Installer Tool - %version%
 goto check_permissions
 
 :check_permissions
-echo.Windows CLI Installer Tool
-echo.Version: %version%
+echo.Windows CLI Installer Tool - %version%
 echo.
 
-echo.Checking user permissions . . .
+echo.:: Checking user permissions . . .
 net session >nul 2>&1
 
 if %errorlevel% == 0 (
     goto main
 ) else (
-    echo.
     echo.ERROR: You need Administrator privileges to use this tool.
     timeout /t 10 /nobreak
     exit 1
@@ -32,7 +30,7 @@ set /p disk=Specify the disk number to format:
 
 if errorlevel 1 (
     echo.
-    echo.Error: Disk %Disk%: Invalid or non-existent disk
+    echo.Error: Could not find disk %disk%, make sure it exists and try again.
     echo.
     timeout /t 5 /nobreak
     exit 
@@ -42,6 +40,7 @@ if errorlevel 1 (
 
 :disk_partitioning
 cls
+echo.:: Partitioning disk %disk%, please wait...
 (
     echo select disk %disk% 
     echo clean
@@ -55,7 +54,7 @@ cls
     echo select partition 4
     echo format fs=ntfs quick
     echo assign letter j
-) | diskpart
+) | diskpart >nul 2>&1
 
 if errorlevel 1 (
     echo.
@@ -64,22 +63,45 @@ if errorlevel 1 (
     pause
     exit 1
 ) else (
-    goto install
+    goto check_installation_files
 )
-echo.
 
-:install
+:check_installation_files
 cls
-rem Ask the user for the desired Windows edition
-set /p source_drive=Specify the drive letter where the Windows installation sources are located at [e.g.: X:\]: 
-dism /get-imageinfo /imagefile:%source_drive%\sources\install.wim
+echo.:: Where is the Windows installation disc mounted at?
+set /p source_drive=Specify the drive letter [e.g. x:\]: 
+
+if exist %source_drive%\sources\install.esd (
+    dism /get-imageinfo /imagefile:%source_drive%\sources\install.esd
+    goto install_esd
+) else (
+    echo.WARNING: Windows installation file "install.esd" not found, retrying with "install.wim"...
+)
+
+if exist %source_drive%\sources\install.wim (
+    dism /get-imageinfo /imagefile:%source_drive%\sources\install.wim
+    goto install_wim
+) else (
+    echo.ERROR: No valid Windows installation file was found, make sure the ISO is mounted and the source drive "%source_drive%\" is correct.
+    timeout /5 /nobreak
+    exit 1
+)
+
+:install_esd
 echo.
-set /p windows_edition=Select the edition (e.g. index number: 2) you want to install: 
+echo.:: What edition of Windows are you looking for?
+set /p windows_edition=Specify the index number: 
+dism /apply-image /imagefile:%source_drive%\sources\install.esd /applydir:j:\ /index:%windows_edition% /verify
+goto install_boot_files
 
-rem Apply the Windows image file to the primary partition in J:\
+:install_wim
+echo.
+echo.:: What edition of Windows are you looking for?
+set /p windows_edition=Specify the index number: 
 dism /apply-image /imagefile:%source_drive%\sources\install.wim /applydir:j:\ /index:%windows_edition% /verify
+goto install_boot_files
 
-rem Install boot files to EFI partition in Z:
+:install_boot_files
 echo.
 bcdboot j:\windows /s i: /f UEFI /v
 pause
