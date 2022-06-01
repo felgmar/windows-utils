1e197508008cca31512b4dc77835f6a8eccf2a75
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2022-06-01 14:07:40 +0100

    Major overhaul
    - Minor indentation tweaks
    - Fix paths
    - Improve script behavior by splitting script into three functions 'deleteChromium', 'downloadChromium' and 'installChromium'
    - Create program shortcut at user's desktop for easier access

--- chromium-updater.ps1
+++ chromium-updater.ps1
@@ -13,16 +13,16 @@ Add-Type -AssemblyName System.Windows.Forms
             Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
         }
         No {
-                Write-Error "This script needs administrator privileges to work, try again."
-                Start-Sleep 5
-                Exit
+            Write-Error "This script needs administrator privileges to work, try again."
+            Start-Sleep 5
+            Exit 1
         }
     }
 }
 
 $programName = 'Chromium'
 $zipFile = 'chrome-win.zip'
-$installDir = "$env:ProgramFiles/The Chromium Project"
+$installDir = "$env:ProgramFiles\The Chromium Project"
 
 function unzipFile {
     #
@@ -33,24 +33,68 @@ function unzipFile {
     [System.IO.Compression.ZipFile]::ExtractToDirectory($zipFile, $outpath)
 }
 
-if (-not(Test-Path -Path $zipFile -PathType Leaf)) {
-    try {
-        $null = Write-Progress "Downloading the latest Chromium win64 build..."
-        Invoke-WebRequest -Uri https://download-chromium.appspot.com/dl/Win_x64?type=snapshots -OutFile $env:TEMP/$zipFile -UseBasicParsing -ErrorAction Stop | Out-Null
-        if (Test-Path -Path $env:TEMP/$zipFile -PathType Leaf) {
-            Write-Host "'$programName' was downloaded successfully"
-            Write-Progress "Extracting '$programName' to '$installDir'..."
-            unzipFile "$env:TEMP/$zipFile" "$installDir"
-            if (Test-Path -Path "$installDir" -PathType Leaf) {
-                Write-Host "'$programName' was installed correctly at '$installDir'"
-            } else {
-                Write-Error "'$programName' is already installed in the directory '$installDir' "
-            }
+
+function deleteChromium
+{
+    if (Test-Path -Path "$installDir")
+    {
+        try
+        {
+            Write-Host "Looks like Chromium is already installed, removing it..."
+            Remove-Item -LiteralPath "$installDir" -Force -Recurse | Out-Null
+        }
+        catch
+        {
+            throw $_.Exception.Message
         }
     }
-    catch {
-        throw $_.Exception.Message
+
+    if (Test-Path -Path "$env:USERPROFILE\Desktop\Chromium.lnk")
+    {
+        Remove-Item -LiteralPath "$env:USERPROFILE\Desktop\Chromium.lnk" -Force | Out-Null
     }
-} else {
-    Write-Error "File '$zipFile' already exists, delete it and try again..."
 }
+
+function downloadChromium
+{
+    if (-not(Test-Path -Path $zipFile)) {
+        try
+        {
+            $null = Write-Progress "Downloading the latest Chromium win64 build..."
+            $ProgressPreference = 'SilentlyContinue'
+            Invoke-WebRequest -Uri https://download-chromium.appspot.com/dl/Win_x64?type=snapshots -OutFile $env:TEMP\$zipFile -UseBasicParsing -ErrorAction Stop | Out-Null
+            if (Test-Path -Path $env:TEMP\$zipFile -PathType Leaf)
+            {
+                Write-Host "$programName was downloaded successfully to $env:TEMP\$zipFile"
+            }
+        }
+        catch
+        {
+            throw $_.Exception.Message
+        }
+    }
+}
+
+function installChromium
+{
+    if (-not(Test-Path -Path "$installDir"))
+    {
+        Write-Progress "Extracting $programName to $installDir..."
+        unzipFile "$env:TEMP\$zipFile" "$installDir"
+        $ShortcutPath = "$env:USERPROFILE\Desktop\Chromium.lnk"
+        $WSScriptObj = New-Object -ComObject ("WScript.Shell")
+        $Shortcut = $WSScriptObj.CreateShortcut($ShortcutPath)
+        $Shortcut.TargetPath =  "$installDir\chrome-win\chrome.exe"
+        $Shortcut.Save()
+        Write-Host "$programName has been installed at $installDir"
+        Write-Information "A shortcut of Chromium has been created at $env:USERPROFILE\Desktop"
+    }
+    else
+    {
+        Write-Error "$programName is already installed in the directory $installDir"
+    }
+}
+
+deleteChromium
+downloadChromium
+installChromium
