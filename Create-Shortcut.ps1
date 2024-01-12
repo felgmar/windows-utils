f8f772220e5acb7be84e7f3226d332f081ed6aa5
Felipe González Martín <15352345-gfelipe099@users.noreply.gitlab.com>
2024-01-12 16:46:25 +0000

    Add a parameter for $WorkingDirectory If it is not define just use $TargetPath otherwise use whatever is defined in $WorkingDirectory
    Signed-off-by: Felipe González Martín <15352345-gfelipe099@users.noreply.gitlab.com>

--- Create-Shortcut.ps1
+++ Create-Shortcut.ps1
@@ -1,20 +1,30 @@
 [CmdletBinding()]
 param (
    [Parameter(Mandatory = $true)]
-   [String] $ProgramName,
+   [String]$ProgramName,
 
    [Parameter(Mandatory = $true)]
-   [String] $ShortcutPath,
+   [String]$ShortcutPath,
+
+   [Parameter(Mandatory = $true)]
+   [String]$TargetPath,
 
    [Parameter(Mandatory = $false)]
-   [String] $TargetPath
+   [String]$WorkingDirectory
 )
 
 process {
     $Shell = New-Object -ComObject ("WScript.Shell")
     $Shortcut = $Shell.CreateShortcut($env:USERPROFILE + "\Desktop\$ProgramName.lnk")
     $Shortcut.TargetPath = $TargetPath
-    #$Shortcut.WorkingDirectory = $TargetPath.Trim("\$ProgramName.exe")
+    if (-not($WorkingDirectory))
+    {
+        $Shortcut.WorkingDirectory = $TargetPath.TrimEnd($ProgramName)
+    }
+    else
+    {
+        $Shortcut.WorkingDirectory = $WorkingDirectory
+    }
     $Shortcut.IconLocation = "$TargetPath, 0"
     $Shortcut.Save()
 }
