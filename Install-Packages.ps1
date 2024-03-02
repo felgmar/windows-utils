4a52f8b63264f679e40f2d7809943195c9cc0516
Ken Hoo <158048821+mrkenhoo@users.noreply.github.com>
2024-03-02 09:52:58 +0000

    Add new utility

---
+++ Install-Packages.ps1
@@ -0,0 +1,44 @@
+[CmdletBinding()]
+param (
+   [Parameter(Mandatory = $false)]
+   [String]$Package
+)
+
+process {
+    if (-not($Package))
+    {
+        $UWPPackages = @(
+            "9MZ1SNWT0N5D",   # PowerShell
+            "9NCVDN91XZQP",   # Python 3.12
+            "9NCBCSZSJRSB",   # Spotify
+            "9NKSQGP7F2NH",   # WhatsApp
+            "XP89DCGQ3K6VLD"  # PowerToys
+        )
+
+        $PackagesList = @(
+            "Bitwarden.Bitwarden",
+            "GitHub.GitHubDesktop",
+            "GitHub.cli",
+            "Git.Git",
+            "JRSoftware.InnoSetup",
+            "PPSSPPTeam.PPSSPP"
+            "Valve.Steam",
+            "Microsoft.VisualStudio.2022.Community",
+            "7zip.7zip",
+            "Telegram.TelegramDesktop",
+            "Microsoft.VisualStudioCode",
+            "ElectronicArts.EADesktop",
+            "Klocman.BulkCrapUninstaller"
+        )
+
+        foreach ($package in $UWPPackages) {
+            Write-Host "Installing package: $package"
+            Start-Process -FilePath "winget.exe" -ArgumentList ("install", "--exact", "--id", $package) -NoNewWindow -Wait
+        }
+
+        foreach ($package in $PackagesList) {
+            Write-Host "Installing package: $package"
+            Start-Process -FilePath "winget.exe" -ArgumentList ("install", "--exact", "--id", $package) -NoNewWindow -Wait
+        }
+    }
+}
