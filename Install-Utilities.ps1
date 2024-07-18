925d75fcbf3e82c5b8f316ee8b78a81fcaff6e0f
Felipe González Martín <158048821+mrkenhoo@users.noreply.github.com>
2024-07-18 22:51:40 +0100

    add new utility

---
+++ Install-Utilities.ps1
@@ -0,0 +1,52 @@
+﻿[CmdletBinding()]
+param (
+   [Parameter(Mandatory = $false)]
+   [String]$DestinationPath
+)
+
+process {
+    $url = 'https://www.uwe-sieber.de/files'
+    $filenames = @('DriveCleanup.zip', 'DeviceCleanupCmd.zip')
+
+    try {
+        $filenames.ForEach({
+            [String]$file = "$url/$_"
+            [String]$file_no_extension = "$_".Replace(".zip", "")
+            
+            if (Test-Path -LiteralPath "$env:SystemDrive\$file_no_extension.exe")
+            {
+                Write-Error -Message "The file $file_no_extension.exe already exists in $env:SystemDrive\."
+            }
+            else
+            {
+                Write-Host "Downloading file $file..."
+                Invoke-WebRequest -Uri "$file" -OutFile "$env:SystemDrive\$_" -ErrorAction Stop
+
+                Write-Host "Extracting file $_... to $env:SystemDrive\"
+                Expand-Archive -Path "$env:SystemDrive\$_" -DestinationPath "$env:SystemDrive\"
+
+                if (Test-Path -LiteralPath "$env:SystemDrive\Win32")
+                {
+                    Remove-Item -LiteralPath "$env:SystemDrive\Win32" -Recurse -Force
+                }
+
+                if (Test-Path -LiteralPath "$env:SystemDrive\x64\$file_no_extension.exe")
+                {
+                    Move-Item -LiteralPath "$env:SystemDrive\x64\$file_no_extension.exe" -Destination "$env:SystemDrive\"
+                    Remove-Item -LiteralPath "$env:SystemDrive\x64" -Recurse -Force
+                }
+
+                if (Test-Path -LiteralPath "$env:SystemDrive\$file_no_extension.txt")
+                {
+                    Remove-Item -LiteralPath "$env:SystemDrive\$file_no_extension.txt" -Force
+                }
+
+                Write-Host "Removing file $_..."
+                Remove-Item -Path "$env:SystemDrive\$_"
+            }
+        })
+    }
+    catch {
+        Write-Error -Message $_.Exception.Message -Exception $_.Exception -Category $_.CategoryInfo.Category
+    }
+}
