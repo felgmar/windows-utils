29c7df3f3573df7e8082a21129bd96777f5e5c64
Felipe González Martín <158048821+felgmar@users.noreply.github.com>
2025-05-10 10:26:10 +0100

    Remove unnecessary quotes and brackets

--- Enable-DEP.ps1
+++ Enable-DEP.ps1
@@ -8,15 +8,16 @@ process {
     try {
         $BCDEntries | ForEach-Object {
             if ($_ -eq 'OptOut') {
-                throw "Data Execution Prevention (DEP) is already set to $_."
+                Write-Warning -Message "Data Execution Prevention (DEP) is already set to $_."
+                return
             }
 
-            $PreviousState = "$_"
-            Write-Host "Changing Data Execution Prevention (DEP) from ${PreviousState} to OptOut..." -ForegroundColor Yellow
+            $PreviousState = $_
+            Write-Host "Changing Data Execution Prevention (DEP) from $PreviousState to OptOut..." -ForegroundColor Yellow
             Start-Process -FilePath "bcdedit.exe" -ArgumentList ('/set', '{current}', 'nx', "OptOut") -NoNewWindow -Wait -PassThru
 
             if ($LASTEXITCODE -ne 0) {
-                throw "Failed to enable Data Execution Prevention (DEP). Exit code: ${LASTEXITCODE}"
+                throw "Failed to enable Data Execution Prevention (DEP). Exit code: $LASTEXITCODE"
             }
 
             Write-Host "Data Execution Prevention (DEP) has been successfully enabled." -ForegroundColor Green
