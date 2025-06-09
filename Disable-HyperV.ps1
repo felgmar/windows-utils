ca8bbd121d0bda84583a232de5070e1e13931f18
Felipe González Martín <158048821+felgmar@users.noreply.github.com>
2025-06-09 21:47:13 +0100

    Add scripts to enable/disable Hyper-V

---
+++ Disable-HyperV.ps1
@@ -0,0 +1,33 @@
+#requires -RunAsAdministrator
+
+process {
+    $BCDEntries = (bcdedit /enum | Select-String -Pattern 'hypervisorlaunchtype') | ForEach-Object {
+        $_.ToString().Trim() -replace ('hypervisorlaunchtype\s+(\w+)', '$1')
+    }
+
+    try {
+        $BCDEntries | ForEach-Object {
+            if ($_ -eq 'Auto') {
+                Write-Warning -Message "Hypervisor launch type is already set to $_."
+                return
+            }
+
+            $PreviousState = $_
+            Write-Host "Changing Hypervisor launch type from $PreviousState to Auto..." -ForegroundColor Yellow
+            Start-Process -FilePath "bcdedit.exe" -ArgumentList ('/set', '{current}', 'hypervisorlaunchtype', "Auto") -NoNewWindow -Wait -PassThru
+
+            if ($LASTEXITCODE -ne 0) {
+                throw "Failed to enable Hypervisor launch type. Exit code: $LASTEXITCODE"
+            }
+
+            Write-Host "Hypervisor launch type has been successfully enabled." -ForegroundColor Green
+            Write-Warning -Message "For the changes to take effect you must reboot your computer."
+            $RebootComputer = Read-Host -Prompt "Do you want to reboot your computer now? <y/n>"
+            if ($RebootComputer -match '^y(es)?$') {
+                shutdown.exe /r /t 5
+            }
+        }
+    } catch {
+        throw $_.Exception.Message
+    }
+}
