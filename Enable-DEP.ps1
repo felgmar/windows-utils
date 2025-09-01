f35dd159b6cc3b5512829ce6d0cba46fa231abfb
Felipe González Martín <158048821+felgmar@users.noreply.github.com>
2025-09-01 18:39:41 +0100

    Enable-DEP: improve readability

--- Enable-DEP.ps1
+++ Enable-DEP.ps1
@@ -15,17 +15,17 @@ process {
             $PreviousState = $_
             Write-Host "Changing Data Execution Prevention (DEP) from $PreviousState to OptOut..." -ForegroundColor Yellow
             Start-Process -FilePath "bcdedit.exe" -ArgumentList ('/set', '{current}', 'nx', "OptOut") -NoNewWindow -Wait -PassThru
+        }
 
-            if ($LASTEXITCODE -ne 0) {
-                throw "Failed to enable Data Execution Prevention (DEP). Exit code: $LASTEXITCODE"
-            }
+        if ($LASTEXITCODE -ne 0) {
+            throw "Failed to enable Data Execution Prevention (DEP). Exit code: $LASTEXITCODE"
+        }
 
-            Write-Host "Data Execution Prevention (DEP) has been successfully enabled." -ForegroundColor Green
-            Write-Warning -Message "For the changes to take effect you must reboot your computer."
-            $RebootComputer = Read-Host -Prompt "Do you want to reboot your computer now? <y/n> "
-            if ($RebootComputer -match '^y(es)?$') {
-                shutdown.exe /r /t 5
-            }
+        Write-Host "Data Execution Prevention (DEP) has been successfully enabled." -ForegroundColor Green
+        Write-Warning -Message "For the changes to take effect you must reboot your computer."
+        $RebootComputer = Read-Host -Prompt "Do you want to reboot your computer now? <y/n> "
+        if ($RebootComputer -match '^y(es)?$') {
+            shutdown.exe /r /t 5
         }
     }
     catch {
