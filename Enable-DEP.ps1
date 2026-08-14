bfbb95e3fe2924d5baf484d213a447c3278b9cff
Felipe González Martín <158048821+felgmar@users.noreply.github.com>
2026-08-14 21:14:18 +0100

    Remove process {} block

--- Enable-DEP.ps1
+++ Enable-DEP.ps1
@@ -1,34 +1,32 @@
 #requires -RunAsAdministrator
 
-process {
-    $BCDEntries = (bcdedit.exe /v | Select-String -Pattern 'nx') | ForEach-Object {
-        $_.ToString().Trim() -replace ('nx\s+(\w+)', '$1')
-    }
+$BCDEntries = (bcdedit.exe /v | Select-String -Pattern 'nx') | ForEach-Object {
+    $_.ToString().Trim() -replace ('nx\s+(\w+)', '$1')
+}
 
-    try {
-        $BCDEntries | ForEach-Object {
-            if ($_ -eq 'OptOut') {
-                Write-Warning -Message "Data Execution Prevention (DEP) is already set to $_."
-                exit 0
-            }
-
-            $PreviousState = $_
-            Write-Host "Changing Data Execution Prevention (DEP) from $PreviousState to OptOut..." -ForegroundColor Yellow
-            Start-Process -FilePath "bcdedit.exe" -ArgumentList ('/set', '{current}', 'nx', "OptOut") -NoNewWindow -Wait -PassThru
+try {
+    $BCDEntries | ForEach-Object {
+        if ($_ -eq 'OptOut') {
+            Write-Warning -Message "Data Execution Prevention (DEP) is already set to $_."
+            exit 0
         }
 
-        if ($LASTEXITCODE -ne 0) {
-            throw "Failed to enable Data Execution Prevention (DEP). Exit code: $LASTEXITCODE"
-        }
+        $PreviousState = $_
+        Write-Host "Changing Data Execution Prevention (DEP) from $PreviousState to OptOut..." -ForegroundColor Yellow
+        Start-Process -FilePath "bcdedit.exe" -ArgumentList ('/set', '{current}', 'nx', "OptOut") -NoNewWindow -Wait -PassThru
+    }
 
-        Write-Host "Data Execution Prevention (DEP) has been successfully enabled." -ForegroundColor Green
-        Write-Warning -Message "For the changes to take effect you must reboot your computer."
-        $RebootComputer = Read-Host -Prompt "Do you want to reboot your computer now? <y/n> "
-        if ($RebootComputer -match '^y(es)?$') {
-            shutdown.exe /r /t 5
-        }
+    if ($LASTEXITCODE -ne 0) {
+        throw "Failed to enable Data Execution Prevention (DEP). Exit code: $LASTEXITCODE"
     }
-    catch {
-        throw $_.Exception.Message
+
+    Write-Host "Data Execution Prevention (DEP) has been successfully enabled." -ForegroundColor Green
+    Write-Warning -Message "For the changes to take effect you must reboot your computer."
+    $RebootComputer = Read-Host -Prompt "Do you want to reboot your computer now? <y/n> "
+    if ($RebootComputer -match '^y(es)?$') {
+        shutdown.exe /r /t 5
     }
+}
+catch {
+    throw $_
 }
